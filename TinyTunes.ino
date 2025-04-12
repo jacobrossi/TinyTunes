6a6dfc8907fcbb094adda47c3bd383c67db77893
jacobrossi <gtjrossi@gmail.com>
2025-04-12 16:58:57 -0700

    Code cleanup

--- TinyTunes.ino
+++ TinyTunes.ino
@@ -3,18 +3,12 @@
 #include "SdFat.h"
 #include "SPI.h"
 #include <ESP32-HUB75-MatrixPanel-I2S-DMA.h> 
-#include <Adafruit_ImageReader.h> // Image-reading functions
+#include <Adafruit_ImageReader.h> 
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include "PrivateConfig.h"
 
-// CONFIGURABLE SETTINGS ---------------------------------------------------
-#define WIDTH  64             // Matrix width in pixels
-#define HEIGHT 64             // Matrix height in pixels
-char BMPpath[] = "/img";     // Absolute path to BMPs on CIRCUITPY drive
-uint16_t BMPminimumTime = 10; // Min. repeat time (seconds) until next BMP
-
-// FLASH FILESYSTEM STUFF --------------------------------------------------
+// FLASH FILESYSTEM STUFF -----------------------------------------------------------
 
 // External flash macros for QSPI or SPI are defined in board variant file.
 #if defined(ARDUINO_ARCH_ESP32)
@@ -33,7 +27,7 @@ FatFileSystem filesys;     // Filesystem object from SdFat
 Adafruit_ImageReader reader(filesys); 
 Adafruit_USBD_MSC usb_msc; // USB mass storage object
 
-// LED Matrix pinout config - default is for Adafruit Matrix Portal S3
+// LED MATRIX CONFIG - default is for Adafruit Matrix Portal S3 --------------------
 #define R1_PIN 42
 #define G1_PIN 41
 #define B1_PIN 40
@@ -56,7 +50,7 @@ HUB75_I2S_CFG::i2s_pins _pins={R1_PIN, G1_PIN, B1_PIN, R2_PIN, G2_PIN, B2_PIN, A
 #define PANEL_RES_Y 64     // Number of pixels tall of each INDIVIDUAL panel module.
 #define PANEL_CHAIN 1      // Total number of panels chained one to another
  
-//MatrixPanel_I2S_DMA dma_display;
+// MatrixPanel_I2S_DMA dma_display;
 MatrixPanel_I2S_DMA *dma_display = nullptr;
 
 uint16_t myBLACK = dma_display->color565(0, 0, 0);
@@ -67,8 +61,7 @@ uint16_t myBLUE = dma_display->color565(0, 0, 255);
 
 File bmpFile;
 
-// FUNCTIONS REQUIRED FOR USB MASS STORAGE ---------------------------------
-
+// USB MASS STORAGE HELPERS --------------------------------------------------------
 static bool msc_changed = true; // Is set true on filesystem changes
 
 // Callback on READ10 command.
@@ -90,58 +83,13 @@ void msc_flush_cb(void) {
   msc_changed = true;
 }
 
-// Get number of files in a specified path that match extension ('filter').
-// Pass in absolute path (e.g. "/" or "/img") and extension WITHOUT period
-// (e.g. "bmp", NOT ".bmp").
-int16_t numFiles(const char *path, const char *filter) {
-  File dir = filesys.open(path);
-  if (!dir) return -1;
-  char filename[256];
-  for(int16_t num_files = 0;;) {
-    File entry = dir.openNextFile();
-    if (!entry) return num_files; // No more files
-    entry.getName(filename, sizeof(filename) - 1);
-    entry.close();
-    if (!entry.isDirectory() &&       // Skip directories
-        strncmp(filename, "._", 2)) { // and Mac junk files
-      char *extension = strrchr(filename, '.');
-      if (extension && !strcasecmp(&extension[1], filter)) num_files++;
-    }
-  }
-  return -1;
-}
-
-// Return name of file (matching extension) by index (0 to numFiles()-1)
-char *filenameByIndex(const char *path, const char *filter, int16_t index) {
-  static char filename[256]; // Must be static, we return a pointer to this!
-  File entry, dir = filesys.open(path);
-  if (!dir) return NULL;
-  while(entry = dir.openNextFile()) {
-    entry.getName(filename, sizeof(filename) - 1);
-    entry.close();
-    if(!entry.isDirectory() &&       // Skip directories
-       strncmp(filename, "._", 2)) { // and Mac junk files
-      char *extension = strrchr(filename, '.');
-      if (extension  && !strcasecmp(&extension[1], filter)) {
-        if(!index--) {
-          return filename;
-        }
-      }
-    }
-  }
-  return NULL;
-}
-
+// GRAPHICS HELPERS ----------------------------------------------------------------
 GFXcanvas16* bmpcanvas;
 
 void drawBMPFile(char *fname) {
   ImageReturnCode stat; // Status from image-reading functions
   Adafruit_Image img;
   stat = reader.loadBMP(fname, img);
-  reader.printStatus(stat);
-
-  
-  Serial.printf("Size: %d,%d",img.width(), img.height());
   bmpcanvas = static_cast<GFXcanvas16*>(img.getCanvas());
   uint16_t pixel; 
   for(int x=0; x<img.width(); x++) {
@@ -152,7 +100,7 @@ void drawBMPFile(char *fname) {
   }
 }
 
-/************************* Arduino Sketch Setup and Loop() *******************************/
+// ARDUINO SETUP FUNCTION -----------------------------------------------------------
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 #if defined(BACK_BUTTON)
@@ -162,7 +110,7 @@ void setup() {
   pinMode(NEXT_BUTTON, INPUT_PULLUP);
 #endif
 
-// USB mass storage / filesystem setup (do BEFORE Serial init)
+  // USB mass storage / filesystem setup (do BEFORE Serial init)
   flash.begin();
   // Set disk vendor id, product id and revision
   usb_msc.setID("Adafruit", "External Flash", "1.0");
@@ -188,21 +136,15 @@ void setup() {
   dma_display->setBrightness8(128); //0-255
   dma_display->clearScreen();
   dma_display->fillScreen(myWHITE);
-  
   dma_display->begin();
   
-  /* all other pixel drawing functions can only be called after .begin() */
+  // Draw Boot Logo
   drawBMPFile("/img/TinyTunes.bmp");
-
   delay(1000); 
 }
 
-// LOOP FUNCTION - RUNS REPEATEDLY UNTIL RESET / POWER OFF -----------------
-int16_t BMPindex = -1;     // Current file index in GIFpath
-int8_t BMPincrement = 1;   // +1 = next GIF, -1 = prev, 0 = same
-
+// ARDUINO MAIN LOOP ------------------------------------------------------------------
 void loop() 
 {
- 
 delay(5000);
 }
